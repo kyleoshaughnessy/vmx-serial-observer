f82553eac5471aeb31a33a177c5203e513d7b907
Kyle O'Shaughnessy <kyle.oshaughnessy@gmail.com>
2023-10-06 13:17:22 -0700

    Added stm32 toolchain and boilerplate build for client application.

---
+++ src/client/main.c
@@ -0,0 +1,9 @@
+/* ==== Application Includes =============================================== */
+#include "serial_interface/vmx_serial_if.h"
+
+/* ==== Functions ========================================================== */
+int main(void)
+{
+    for (;;);
+    return 0;
+}
