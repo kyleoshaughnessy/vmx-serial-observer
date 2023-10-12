ea0c00009fc1068f6ac85fd152c827e40996cd55
Kyle O'Shaughnessy <kyle.oshaughnessy@gmail.com>
2023-10-11 17:12:12 -0700

    Add some sample uart and gpio code to client app. Temporarilly including STM32CubeIDE example stubbed code that will be refactored later.

--- src/client/main.c
+++ src/client/main.c
@@ -1,47 +1,127 @@
 /* ==== Application Includes =============================================== */
 #include "serial_interface/vmx_serial_if.h"
 #include <stm32f4xx_hal.h>
-
-// We Act Studio STM32F411 “BlackPill” Development Board - PC13
-#define LED_PORT                GPIOC
-#define LED_PIN                 GPIO_PIN_13
-#define LED_PORT_CLK_ENABLE     __HAL_RCC_GPIOC_CLK_ENABLE
+#include <stdio.h>
 
 /* ==== Functions ========================================================== */
-void SysTick_Handler(void)
+static void Error_Handler(void)
 {
-    HAL_IncTick();
-
-    // 1 Hz blinking
-    if ((HAL_GetTick() % 500) == 0)
+    __disable_irq();
+    while (1)
     {
-        HAL_GPIO_TogglePin(LED_PORT, LED_PIN);
     }
 }
 
-void initGPIO()
+static void initClocks()
 {
-    GPIO_InitTypeDef GPIO_Config;
+    RCC_OscInitTypeDef rccOscInit = {0};
+    RCC_ClkInitTypeDef rccClkInit = {0};
 
-    GPIO_Config.Mode = GPIO_MODE_OUTPUT_PP;
-    GPIO_Config.Pull = GPIO_NOPULL;
-    GPIO_Config.Speed = GPIO_SPEED_FREQ_HIGH;
+    // Enable PWR clk
+    __HAL_RCC_PWR_CLK_ENABLE();
 
-    GPIO_Config.Pin = LED_PIN;
+    // Configure the main internal regulator output voltage
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-    LED_PORT_CLK_ENABLE();
-    HAL_GPIO_Init(LED_PORT, &GPIO_Config);
+    // Initialize RCC oscillators
+    rccOscInit.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+    rccOscInit.HSEState = RCC_HSE_ON;
+    rccOscInit.PLL.PLLState = RCC_PLL_ON;
+    rccOscInit.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    rccOscInit.PLL.PLLM = 25;
+    rccOscInit.PLL.PLLN = 192;
+    rccOscInit.PLL.PLLP = RCC_PLLP_DIV2;
+    rccOscInit.PLL.PLLQ = 4;
+    if (HAL_RCC_OscConfig(&rccOscInit) != HAL_OK)
+    {
+        Error_Handler();
+    }
+
+    // Initialize CPU, AHB and APB clocks
+    rccClkInit.ClockType      = RCC_CLOCKTYPE_HCLK   |
+                                RCC_CLOCKTYPE_SYSCLK |
+                                RCC_CLOCKTYPE_PCLK1  |
+                                RCC_CLOCKTYPE_PCLK2;
+    rccClkInit.SYSCLKSource   = RCC_SYSCLKSOURCE_PLLCLK;
+    rccClkInit.AHBCLKDivider  = RCC_SYSCLK_DIV1;
+    rccClkInit.APB1CLKDivider = RCC_HCLK_DIV2;
+    rccClkInit.APB2CLKDivider = RCC_HCLK_DIV1;
+    if (HAL_RCC_ClockConfig(&rccClkInit, FLASH_LATENCY_3) != HAL_OK)
+    {
+        Error_Handler();
+    }
+}
+
+static void initGPIO()
+{
+    GPIO_InitTypeDef gpioInit = { 0 };
+
+    // Enabled clocks for GPIO ports
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+    __HAL_RCC_GPIOH_CLK_ENABLE();
+
+    gpioInit.Pin   = GPIO_PIN_13;
+    gpioInit.Mode  = GPIO_MODE_OUTPUT_PP;
+    gpioInit.Pull  = GPIO_NOPULL;
+    gpioInit.Speed = GPIO_SPEED_FREQ_LOW;
+
+    HAL_GPIO_Init(GPIOC, &gpioInit);
+}
+
+static void initUART(UART_HandleTypeDef *pUARTHandle)
+{
+    pUARTHandle->Instance          = USART1;
+    pUARTHandle->Init.BaudRate     = 115200;
+    pUARTHandle->Init.WordLength   = UART_WORDLENGTH_8B;
+    pUARTHandle->Init.StopBits     = UART_STOPBITS_1;
+    pUARTHandle->Init.Parity       = UART_PARITY_NONE;
+    pUARTHandle->Init.Mode         = UART_MODE_TX_RX;
+    pUARTHandle->Init.HwFlowCtl    = UART_HWCONTROL_NONE;
+    pUARTHandle->Init.OverSampling = UART_OVERSAMPLING_16;
+
+    if (HAL_UART_Init(pUARTHandle) != HAL_OK)
+    {
+        Error_Handler();
+    }
 }
 
 int main(void)
 {
+    UART_HandleTypeDef uartHandle =  { 0 };
+    uint8_t msgOut[25]  = {'\0'};
+    uint8_t msgIn[25]   = {0};
+    uint8_t numChars;
+
     HAL_Init();
+    initClocks();
     initGPIO();
+    initUART(&uartHandle);
 
-    // 1kHz ticks
-    HAL_SYSTICK_Config(SystemCoreClock / 1000);
+    HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
+    numChars = sprintf(msgOut, "LED ON\r\n");
+    HAL_UART_Transmit(&uartHandle, msgOut, numChars + 1, 100);
 
-    while(1);
+    while (1)
+    {
+        HAL_UART_Receive(&uartHandle, msgIn, sizeof(msgIn), 100);
+
+        if (msgIn[0] == '1')
+        {
+            HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
+            numChars = sprintf(msgOut, "LED OFF\r\n");
+            HAL_UART_Transmit(&uartHandle, msgOut, numChars + 1, 100);
+        }
+        else if (msgIn[0] == '2')
+        {
+            HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
+            numChars = sprintf(msgOut, "LED ON\r\n");
+            HAL_UART_Transmit(&uartHandle, msgOut, numChars + 1, 100);
+        }
+        msgIn[0] = '\0';
+        HAL_Delay(500);
+        // HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+    }
 
     return 0;
 }
